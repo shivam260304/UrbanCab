ea01fb20c15e48a6cc2b9b2bfd3f88d93d7098c4
shivam260304 <shivam260304@gmail.com>
2025-01-03 18:13:06 +0530

    integrated till captaindetail at captainHome day 17 9:00

--- FRONTEND/src/components/CaptainDetails.jsx
+++ FRONTEND/src/components/CaptainDetails.jsx
@@ -1,10 +1,13 @@
+import { useContext} from "react";
+import { CaptainDataContext } from "../context/CaptainContext";
 const CaptainDetails = () => {
+  const { captain } = useContext(CaptainDataContext);
   return (
     <div>
       <div className="flex items-center justify-between">
           <div className="flex items-center justify-start gap-3">
             <img className="h-10 w-10 rounded-full object-cover" src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/35af6a41332353.57a1ce913e889.jpg" alt="" />
-            <h4 className="text-lg font-medium">Shivam Rajput</h4>
+            <h4 className="text-lg font-medium">{captain.fullname.firstname + " " + captain.fullname.lastname}</h4>
           </div>
           <div>
             <h4 className="text-xl font-semibold">Rs 193</h4>
--- FRONTEND/src/pages/CaptainProtectWrapper.jsx
+++ FRONTEND/src/pages/CaptainProtectWrapper.jsx
@@ -12,27 +12,27 @@ const CaptainProtectWrapper = ({ children }) => {
   const{captain, setCaptain} = useContext(CaptainDataContext)
   const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!token) {
-    navigate("/captain-login");
-  }
-  },[token])
+        navigate('/captain-login')
+    }
 
-  axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile`,{
-    headers:{
-        Authorization: `Bearer ${token}`,
-    }
-  }).then((response)=>{
-    if(response.status === 200) {
-        const data = response.data;
-        setCaptain(data.captain);
-        setIsLoading(false);
-    }
-  }).catch((error)=>{
-    console.log(error);
-    localStorage.removeItem('token');
-    navigate("/captain-login");
-  });
+    axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }).then(response => {
+        if (response.status === 200) {
+            setCaptain(response.data.captain)
+            setIsLoading(false)
+        }
+    })
+        .catch(err => {
+
+            localStorage.removeItem('token')
+            navigate('/captain-login')
+        })
+}, [ token ])
 
   if(isLoading) {
     return (<div>Loading...</div>)
